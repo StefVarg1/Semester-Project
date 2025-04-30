18aeffbe0b7ec4a74a13f4bd0bcf63827f28fd40
StefVarg1 <plitzo@live.se>
2025-04-30 15:12:40 -0500

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,18 @@
-Hello World
+#Hello World
+
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+
+st.set_page_config(
+    page_title="Hope Foundation Data",
+    page_icon="📊",
+    layout="wide"
+)
+
+@st.cache_data
+def load_data():
+    return pd.read_csv('UNO Service Learning Data Sheet De-Identified Version.csv', parse_dates=['date'])
+
+data = load_data
