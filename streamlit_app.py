e9af647d17fee08b2fe17d7db0a4a4bf4ce04515
StefVarg1 <plitzo@live.se>
2025-05-01 12:03:31 -0500

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ from datetime import datetime
 st.title('Hope Foundation Data by Stefan')
 
 DATE_COLUMN = 'Grant Req Date'
-DATA_URL = ('https://github.com/StefVarg1/Semester-Project/blob/main/UNO%20Service%20Learning%20Data%20Sheet%20De-Identified%20Version.csv'
+DATA_URL = ('https://github.com/StefVarg1/Semester-Project/raw/refs/heads/main/UNO%20Service%20Learning%20Data%20Sheet%20De-Identified%20Version.csv'
          'UNO Service Learning Data Sheet De-Identified Version.csv')
 
 def load_data(nrows):
@@ -18,11 +18,13 @@ def load_data(nrows):
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
+
     # Create a text element and let the reader know the data is loading.
     data_load_state = st.text('Loading data...')
     # Load 10,000 rows of data into the dataframe.
     data = load_data(1000)
     # Notify the reader that the data was successfully loaded.
     data_load_state.text('Loading data...done!')
+
 
 
