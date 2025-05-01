644cdd0cb0dc4a15cbdb7c5f8c1b4c0dda8e145b
StefVarg1 <plitzo@live.se>
2025-05-01 10:32:01 -0500

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import requests
 from io import BytesIO
-from thefuzz import process
+from fuzzywuzzy import process
 import streamlit as st
 import numpy as np
 import plotly.express as px
