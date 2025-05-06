d083429721d33a99206b13d68df303ac911e3815
StefVarg1 <plitzo@live.se>
2025-05-06 12:00:48 -0500

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,31 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from pathlib import Path
-from datetime import datetime
-
-# -- CONFIGURATION --
-
-st.title('Hope Foundation Data by Stefan')
-
-DATE_COLUMN = 'Grant Req Date'
-DATA_URL = ('https://github.com/StefVarg1/Semester-Project/raw/main/UNO%20Service%20Learning%20Data%20Sheet%20De-Identified%20Version.xlsx'
-         'UNO Service Learning Data Sheet De-Identified Version.csv')
-
-def load_data(nrows):
-    data = pd.read_xlsx(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-    # Create a text element and let the reader know the data is loading.
-    data_load_state = st.text('Loading data...')
-    # Load 10,000 rows of data into the dataframe.
-    data = load_data(1000)
-    # Notify the reader that the data was successfully loaded.
-    data_load_state.text("Done! (using st.cache_data)")
+import plotly.express as px
+from thefuzz import process
+from pandas import series
 
 
+# Functions to help with figuring out the inconsistent data
+
+def fuzzy_map(series: pd.Series, options: list[str], default=np.nan, lower: bool = True) -> pd.Series:
+    """
+    Fuzzy-match every value in the 'series' to the closest possible 'options'.
+    Should return the matched option or 'default' if input is null, nan or can't match.
+    """
+    opts = [opt.lower() if lower else opt for opt in options]
+    def _match(excel_value):
+        if pd.isa(excel_value):
+            return default
+        text = excel_value.lower() if lower else str(excel_value)
+        match, score = process.extractOne(text, opts)
+        return match if score >= 55 else default
+    return series.astype(str).apply(_match)
+
+@st.cache_data
+def import_and_clean(sheet_name=0) -> pd.DataFrame:
+    # 1) Load
+    url = ("https://github.com/StefVarg1/Semester-Project/raw/refs/heads/main/UNO%20Service%20Learning%20Data%20Sheet%20De-Identified%20Version.xlsx")
+    df = pd.read_excel((url), sheet_name=sheet_name)
+    st.write("✅ Data is ready to view!")
 
