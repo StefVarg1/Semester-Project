7155dcff344ce3c5c69b2c288fdd94ddd5cb3145
StefVarg1 <plitzo@live.se>
2025-05-06 13:05:03 -0500

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,19 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from thefuzz import process
+import matplotlib.pyplot as plt
 
-st.title('Hope Foundation Data by Stefan')
+# Load the data into the dataframe and pray it works
+@st.cache_data
+def import_and_clean(sheet_name: int=0) -> pd.DataFrame:
+    url = ("https://github.com/StefVarg1/Semester-Project/raw/refs/heads/main/UNO%20Service%20Learning%20Data%20Sheet%20De-Identified%20Version.xlsx")
+    df = pd.read_excel((url), sheet_name=sheet_name)
+    st.write("✅ Data is ready to view!")
+
+# Streamlit Webpage setup
+    st.title('Hope Foundation Data by Stefan')
+    st.subheader("Hope Foundation Data Preview")
+    st.write(df.head())
 
 # Functions to help with figuring out the inconsistent data
 
@@ -20,13 +31,6 @@ def fuzzy_map(series: pd.Series, options: list[str], default=np.nan, lower: bool
         match, score = process.extractOne(text, opts)
         return match if score >= 55 else default
     return series.astype(str).apply(_match)
-
-@st.cache_data
-def import_and_clean(sheet_name=0) -> pd.DataFrame:
-    # Load the data into the dataframe and pray it works
-    url = ("https://github.com/StefVarg1/Semester-Project/raw/refs/heads/main/UNO%20Service%20Learning%20Data%20Sheet%20De-Identified%20Version.xlsx")
-    df = pd.read_excel((url), sheet_name=sheet_name)
-    st.write("✅ Data is ready to view!")
 
     # Clean the data to have the inconsistent values figured out with thefuzz matching closest option!
     
